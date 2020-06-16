5c73be38c900b28431e50f2caa88418786c9202f
Cesar Whesly Segura <cesarw.segura@gmail.com>
2020-06-16 16:07:21 -0500

    test

--- main.cpp
+++ main.cpp
@@ -7,12 +7,12 @@ using namespace torch::nn;
 int main() {
 
 	std::shared_ptr<torch::jit::script::Module> net; //!< the neural network for face landmarks
-	net = make_shared<torch::jit::script::Module>(torch::jit::load("Retrain_pretrained_model_8epoch.pt", Device("cuda")));
+	net = make_shared<torch::jit::script::Module>(torch::jit::load("/var/fcs_res/MobileNetv2_Adam_01_Eyes_Norm_Without_Flip_v2_70epochs.pt", Device("cuda")));
 	cout << "warming up..." << endl;
 	for(int i = 0; i < 5; i++){
 		net->eval();
 		NoGradGuard guard;
-		torch::Tensor tensor = torch::randn({1,3,160, 160});
+		torch::Tensor tensor = torch::randn({1,3,256, 256});
 		tensor = tensor.to("cuda");
 		vector<torch::jit::IValue> inputs;
 		inputs.push_back(tensor);
