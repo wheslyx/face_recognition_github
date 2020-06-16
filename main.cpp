3f1a97a8c0bbf72d694d37cc9bcb48b62bb1b174
Cesar Whesly Segura <cesarw.segura@gmail.com>
2020-06-16 15:45:24 -0500

    cuda

--- main.cpp
+++ main.cpp
@@ -7,17 +7,17 @@ using namespace torch::nn;
 int main() {
 
 	std::shared_ptr<torch::jit::script::Module> net; //!< the neural network for face landmarks
-	net = make_shared<torch::jit::script::Module>(torch::jit::load("Retrain_pretrained_model_8epoch.pt", Device("cpu")));
+	net = make_shared<torch::jit::script::Module>(torch::jit::load("Retrain_pretrained_model_8epoch.pt", Device("cuda")));
 	cout << "warming up..." << endl;
 	for(int i = 0; i < 5; i++){
 		net->eval();
 		NoGradGuard guard;
 		torch::Tensor tensor = torch::randn({1,3,160, 160});
-		//tensor = tensor.to("cuda");
+		tensor = tensor.to("cuda");
 		vector<torch::jit::IValue> inputs;
 		inputs.push_back(tensor);
 		Tensor output = net->forward(inputs).toTensor();
-		//output = output.to("cpu");
+		output = output.to("cpu");
 		cout << output << endl;
 	}
 	cout << "warm up ended" << endl << endl;
