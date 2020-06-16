5e6d1e868d14286f206388f119ee5c2c4792fd80
Cesar Whesly Segura <cesarw.segura@gmail.com>
2020-06-16 15:19:29 -0500

    debugging

--- main.cpp
+++ main.cpp
@@ -10,20 +10,17 @@ int main() {
 	net = make_shared<torch::jit::script::Module>(torch::jit::load("Retrain_pretrained_model_8epoch.pt", Device("cuda")));
 	cout << "warming up..." << endl;
 	for(int i = 0; i < 5; i++){
-		warmUp();
+		net->eval();
+		NoGradGuard guard;
+		torch::Tensor tensor = torch::randn({1,3,160, 160});
+		tensor = tensor.to("cuda");
+		vector<torch::jit::IValue> inputs;
+		inputs.push_back(tensor);
+		Tensor output = net->forward(inputs).toTensor();
+		output = output.to("cpu");
+		cout << output << endl;
 	}
 	cout << "warm up ended" << endl << endl;
 	return 0;
 }
 
-void warmUp(){
-			net->eval();
-			NoGradGuard guard;
-			torch::Tensor tensor = torch::randn({1,3,160, 160});
-			tensor = tensor.to("cuda");
-			vector<torch::jit::IValue> inputs;
-			inputs.push_back(tensor);
-			Tensor output = net->forward(inputs).toTensor();
-			output = output.to("cpu");
-			cout << output << endl;
-		}
