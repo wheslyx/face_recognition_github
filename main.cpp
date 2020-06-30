d35c13ecbb042c287627a44cd96c7dcceec21cd9
Cesar Whesly Segura <cesarw.segura@gmail.com>
2020-06-30 10:10:23 -0500

    run

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace torch::nn;
 int main() {
 
 	std::shared_ptr<torch::jit::script::Module> net; //!< the neural network for face landmarks
-	net = make_shared<torch::jit::script::Module>(torch::jit::load("/home/tps/face_recognition_github/Retrain_pretrained_model_8epoch.pt", Device("cuda")));
+	net = make_shared<torch::jit::script::Module>(torch::jit::load("/home/tps/FaceNet_Classify_Directly_Frontal_Aligment_7epochs.pt", Device("cuda")));
 	cout << "warming up..." << endl;
 	for(int i = 0; i < 5; i++){
 		net->eval();
