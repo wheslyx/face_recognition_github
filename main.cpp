4b9c887f135d97f5549a1ba455bdd37b6b449015
Cesar Whesly Segura <cesarw.segura@gmail.com>
2020-08-30 19:19:16 -0500

    main debug

--- main.cpp
+++ main.cpp
@@ -25,8 +25,72 @@ int main() {
 	std::string pathInputVideo = "";
 	std::string pathOutputVideo= "/media/disk/video_test/face_recognition.avi";
 
-	recognizer.runVideo(pathInputVideo, pathOutputVideo);
+	// This is the run video attribute
+	cv::VideoCapture cap;
+	if(pathInputVideo.empty()){
+		cap = cv::VideoCapture(0);
+	} else{
+		cap = cv::VideoCapture(pathInputVideo);
+	}
 	
+	if(!cap.isOpened() ){
+		if(pathInputVideo.empty()){
+			std::cout << "Error in camera" << std::endl;
+		} else{
+			std::cout << "No video found" << std::endl;
+		}
+		return;
+	}
+	int frame_width = cap.get(cv::CAP_PROP_FRAME_WIDTH); 
+	int frame_height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+	cv::VideoWriter video;
+	if(!pathOutputVideo.empty()){
+		video = cv::VideoWriter(pathOutputVideo ,cv::VideoWriter::fourcc('M','J','P','G'),10, cv::Size(frame_width,frame_height));
+	} 
+	
+	while(true){
+		cv::Mat frame;
+		cap >> frame;
+		if(frame.empty()){
+			break;
+		}
+		std::vector<cv::Rect> rectFaces = faceDetector.getFaces(frame);
+		cv::Rect rectFace = rectFaces[0];
+		long double startrun = getTime();
+		std::pair<std::string,float> prediction = recognize(frame, rectFace);
+		long double endrun = getTime();
+		std::string name = prediction.first;
+		float recognition_confidence = prediction.second;
+		if(!(name == "no face detected" || recognition_confidence < RECOGNITION_THRESHOLD)){
+			cv::rectangle(frame ,rectFace, cv::Scalar( 255, 0, 0 ));
+			int yDraw = rectFace.y - 10;
+			if(yDraw <= 10){
+				yDraw = rectFace.y + 15;
+			}
+			int xDraw = rectFace.x + 60;
+			if (xDraw >=frame_width){
+				xDraw =rectFace.x - 60;
+			}
+					
+			cv::putText(frame , name, cv::Point(rectFace.x, yDraw),cv::FONT_HERSHEY_SIMPLEX, 0.45, cv::Scalar(0, 255, 0), 2);
+			cv::putText(frame, std::to_string( (int) (recognition_confidence*100)), cv::Point(xDraw, yDraw), cv::FONT_HERSHEY_SIMPLEX, 0.45, cv::Scalar(0,255,0),2);
+		}
+		
+		if(!pathOutputVideo.empty()){
+			video.write(frame);
+		} 
+
+		char c = (char) cv::waitKey(25);
+		if(c == 27 || c == 32){
+			break;
+		}
+		std::cout << "FPS = " << std::fixed << std::setprecision(1) << 1000/(endrun - startrun) << std::endl;
+	}
+	cap.release();
+	if(!pathOutputVideo.empty()){
+		video.release();
+	} 
+	cv::destroyAllWindows();	
 
 	return 0;
 }
